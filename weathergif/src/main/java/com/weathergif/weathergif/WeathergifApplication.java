d5a9cabc3ff66b9ceb047452ac9f5dd128df116b
warpiller <74993082+warpiller@users.noreply.github.com>
2024-08-18 01:20:17 +0300

    Initial commit

---
+++ weathergif/src/main/java/com/weathergif/weathergif/WeathergifApplication.java
@@ -0,0 +1,13 @@
+package com.weathergif.weathergif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeathergifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeathergifApplication.class, args);
+	}
+
+}
---
+++ weathergif/src/test/java/com/weathergif/weathergif/WeathergifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weathergif.weathergif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeathergifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
