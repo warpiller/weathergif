d465002258f646b55a9d088b9af975e684cc124f
Alamart1683 <43591047+Alamart1683@users.noreply.github.com>
2024-08-21 01:26:40 +0300

    Add new gif uploading

--- src/main/java/com/weathergif/weathergif/controller/WeatherController.java
+++ src/main/java/com/weathergif/weathergif/controller/WeatherController.java
@@ -6,10 +6,20 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -23,16 +33,25 @@ public class WeatherController {
     }
 
     @SneakyThrows
-    @GetMapping("/weather/gif")
-    public void getWeatherGif(@RequestParam String city, HttpServletResponse response) {
-        String source = weatherService.getWeatherGif(city);
-        URL url = new URL(source);
-        URLConnection conn = url.openConnection();
-        conn.setConnectTimeout(5000);
-        conn.setReadTimeout(5000);
-        conn.connect();
-        response.setContentType("image/gif");
-        response.addHeader("Content-Disposition", "attachment; filename=" + source + ".gif");
-        IOUtils.copy(conn.getInputStream(), response.getOutputStream());
+    @GetMapping(value = "/weather/gif", produces = "image/gif")
+    public @ResponseBody ResponseEntity<ByteArrayResource> getWeatherGif(@RequestParam String city, HttpServletResponse response) {
+        String gifUrl = weatherService.getWeatherGif(city); // Замените на URL вашего GIF
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Скачиваем GIF
+        byte[] gifBytes;
+        try {
+            gifBytes = restTemplate.getForObject(gifUrl, byte[].class);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+        // Возвращаем GIF пользователю
+        ByteArrayResource resource = new ByteArrayResource(gifBytes);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"downloaded.gif\"")
+                .contentType(MediaType.IMAGE_GIF)
+                .contentLength(gifBytes.length)
+                .body(resource);
     }
 }
